c976419418eb10a4243a3abc732b7a8a95ddbb8a
BTSL-JAHID\jahid.hasan <jahid.hasan@btracsl.com>
2022-10-11 11:39:54 +0600

    7th commit

--- my-activity/src/components/Main/Main.js
+++ my-activity/src/components/Main/Main.js
@@ -2,15 +2,3 @@ import React from 'react';
 import logo from '../../images/logo.svg'
 import './Main.css'
 
-const Main = () => {
-    return (
-        <div>
-        <nav className='header'>    
-            <img src={logo} alt="" ></img>
-            
-        </nav>
-      </div>
-    );
-};
-
-export default Main;
