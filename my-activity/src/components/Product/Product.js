d3b973b4bac53c776fdda6b8d3deff31c7872195
BTSL-JAHID\jahid.hasan <jahid.hasan@btracsl.com>
2022-10-11 11:46:04 +0600

    10th Commit

--- my-activity/src/components/Product/Product.js
+++ my-activity/src/components/Product/Product.js
@@ -8,4 +8,29 @@ const Product = ({product, handleCart}) => {
     // console.log(props.product)
     // const {handleClick}=props;
    
+ 
+    const {name, img, dis, age, time} = product;
+    // console.log(props)
+   
+    return (
+        <div>
+            
+        <div className='product'>
+        <img src={img} alt="" />
+        <div className='info'>
+        <p className='product-name'>{name}</p>
+        <p>{dis}</p>
+        <div className='another'>
+        <p>For Age: {age}</p>
+        <p>Time required: {time}s </p>
+        </div>
+        </div>
 
+        <button onClick={() => handleCart(product)} className="btn btn-primary m-5">Add to List</button>
+
+        </div>
+        </div>
+    );
+};
+
+export default Product;
